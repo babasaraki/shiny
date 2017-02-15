70e7ab8c4e058d66c2e153dbbf3d6bbe55f40526
thebioengineer <ellishughes@live.com>
2017-02-15 14:06:13 -0800

    Adding console Calls to output_binding_text

--- srcjs/output_binding_text.js
+++ srcjs/output_binding_text.js
@@ -1,9 +1,11 @@
 var textOutputBinding = new OutputBinding();
 $.extend(textOutputBinding, {
   find: function(scope) {
+    console.log('find textoutputbinding')
     return $(scope).find('.shiny-text-output');
   },
   renderValue: function(el, data) {
+    console.log('renderValue textoutputbinding')
     $(el).text(data);
   }
 });
